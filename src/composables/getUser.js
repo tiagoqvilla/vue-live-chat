68e2528379e8cce4fe0881119ea43e57748f7f69
Tiago Villa <tiago_villa@hotmail.com>
2022-09-02 18:52:29 -0300

    Add chat window component

--- src/composables/getUser.js
+++ src/composables/getUser.js
@@ -4,7 +4,6 @@ import { ref } from 'vue'
 const user = ref(projectAuth.currentUser)
 
 projectAuth.onAuthStateChanged((_user) => {
-  console.log('user state change. current user is ' + _user)
   user.value = _user
 })
 
--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -2,17 +2,20 @@
     <div class="container">
         <Navbar />
         <NewChatForm />
+        <ChatWindow />
     </div>
 </template>
 
 <script>
 import Navbar from '@/components/Navbar.vue'
 import NewChatForm from '@/components/NewChatForm.vue'
+import ChatWindow from '@/components/ChatWindow.vue'
 import getUser from '@/composables/getUser'
 import {watch} from 'vue'
 import { useRouter } from 'vue-router'
+
     export default {
-        components: { Navbar, NewChatForm },
+        components: { Navbar, NewChatForm, ChatWindow },
         setup() {
             const {user} = getUser()
             const router = useRouter()
